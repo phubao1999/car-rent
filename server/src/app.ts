9892939921a4f1a4ec48ff99563c31b6c36c7990
baophu <phutuongbao1999@gmail.com>
2025-05-02 01:14:30 +0700

    Adding Logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,16 @@
 import cors from 'cors';
 import express from 'express';
+import morgan from 'morgan';
 import { connectDB } from './db';
 import { errorHandler } from './middlewares';
 import { adminRouter, carRouter, userRouter } from './routes';
+import { loggerStream } from './utils/logger.util';
 
 // Connect DB
 connectDB();
 // APP init
 const app = express();
+app.use(morgan('combined', { stream: loggerStream }));
 app.use(express.json());
 app.use(cors());
 
